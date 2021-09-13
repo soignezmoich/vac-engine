cc1323bdc2ef2e48fd70f0647b099af56799b211
Nicolas goy <kuon@goyman.com>
2021-09-14 01:03:16 +0200

    Basic auth by role plug

---
+++ lib/vac_engine_web/plugs/role_plug.ex
@@ -0,0 +1,55 @@
+defmodule VacEngineWeb.RolePlug do
+  import Plug.Conn
+  import Phoenix.Controller, only: [redirect: 2]
+
+  alias VacEngine.Auth
+
+  def fetch_role_session(conn, _) do
+    with {:ok, session} <- restore_session(conn) do
+      conn
+      |> assign(:role_session, session)
+      |> assign(:role, session.role)
+    else
+      _ ->
+        conn
+        |> assign(:role_session, nil)
+        |> assign(:role, nil)
+    end
+  end
+
+  def require_role(conn, _) do
+    if conn.assigns[:role] == nil do
+      conn
+      |> put_session("login_next_url", conn.request_path)
+      |> redirect(to: "/login")
+      |> halt
+    else
+      conn
+    end
+  end
+
+  def require_no_role(conn, _) do
+    if conn.assigns[:role] != nil do
+      conn
+      |> redirect(to: "/")
+      |> halt
+    else
+      conn
+    end
+  end
+
+  defp restore_session(conn) do
+    with ua <- conn |> get_req_header("user-agent"),
+         {:ok, session} <-
+           conn |> get_session("role_session_token") |> Auth.fetch_session(),
+         {:ok, session} <-
+           Auth.update_session(session, %{
+             "remote_ip" => conn.remote_ip,
+             "client_info" => %{"user-agent" => ua}
+           }) do
+      {:ok, session}
+    else
+      _ -> {:error, "cannot fetch an update session"}
+    end
+  end
+end
