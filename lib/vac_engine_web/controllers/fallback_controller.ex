a8093187574cc9b4dec83402ef9f230bc1098f3e
Nicolas goy <kuon@goyman.com>
2021-09-14 01:31:23 +0200

    Make nice error pages

---
+++ lib/vac_engine_web/controllers/fallback_controller.ex
@@ -0,0 +1,57 @@
+defmodule VacEngineWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use VacEngineWeb, :controller
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> set_view()
+    |> render(:"404")
+  end
+
+  def call(conn, {:error, :unauthorized}) do
+    conn
+    |> put_status(:unauthorized)
+    |> set_view()
+    |> render(:"401")
+  end
+
+  def call(conn, {:error, :forbidden}) do
+    conn
+    |> put_status(:forbidden)
+    |> set_view()
+    |> render(:"403")
+  end
+
+  def call(conn, {:error, :bad_request, message}) do
+    conn
+    |> put_status(:bad_request)
+    |> set_view()
+    |> render(:"400", message: message)
+  end
+
+  def call(conn, {:error, :bad_request}) do
+    conn
+    |> put_status(:bad_request)
+    |> set_view()
+    |> render(:"400")
+  end
+
+  def call(conn, {:error, _err}) do
+    conn
+    |> put_status(:internal_server_error)
+    |> set_view()
+    |> render(:"500")
+  end
+
+  defp set_view(conn) do
+    conn
+    |> put_layout(false)
+    |> put_root_layout(false)
+    |> put_view(VacEngineWeb.ErrorView)
+  end
+end
--- lib/vac_engine_web/views/error_view.ex
+++ lib/vac_engine_web/views/error_view.ex
@@ -10,7 +10,8 @@ defmodule VacEngineWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
   # "Not Found".
-  def template_not_found(template, _assigns) do
-    Phoenix.Controller.status_message_from_template(template)
+  def template_not_found(_template, assigns) do
+    render("500.html", assigns)
   end
+
 end
