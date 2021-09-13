8be0b5c380b2cfd382ed8a954ded63bbf3acf09b
Nicolas goy <kuon@goyman.com>
2021-09-14 01:32:05 +0200

    Conn utility

---
+++ lib/vac_engine_web/conn_utils.ex
@@ -0,0 +1,13 @@
+defmodule VacEngineWeb.ConnUtils do
+  import Plug.Conn
+
+  def session_attrs(conn) do
+    ua = conn |> get_req_header("user-agent")
+
+    %{
+      "last_active_at" => NaiveDateTime.utc_now(),
+      "remote_ip" => conn.remote_ip,
+      "client_info" => %{"user-agent" => ua}
+    }
+  end
+end
