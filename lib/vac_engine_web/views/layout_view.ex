801650417288c4be1e72299567a4f5bb2b9d5ae6
Nicolas goy <kuon@goyman.com>
2021-09-14 22:29:44 +0200

    DRY header template

--- lib/vac_engine_web/views/layout_view.ex
+++ lib/vac_engine_web/views/layout_view.ex
@@ -1,4 +1,25 @@
 defmodule VacEngineWeb.LayoutView do
   use VacEngineWeb, :view
 
+  def header_els(:left, %{assigns: %{role: role}} = conn) when not is_nil(role) do
+    [
+      {"Dashboard", Routes.dashboard_path(conn, :index)},
+    ]
+  end
+
+  def header_els(:right, %{assigns: %{role: role}} = conn) when not is_nil(role) do
+    [
+      {"Logout", Routes.auth_path(conn, :logout)},
+    ]
+  end
+
+  def header_els(:left, _conn) do
+    []
+  end
+
+  def header_els(:right, conn) do
+    [
+      {"Login", Routes.auth_login_path(conn, :login)},
+    ]
+  end
 end
