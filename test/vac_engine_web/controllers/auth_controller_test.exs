e7066e0af615bee34fc6fd80394bb889012a49f2
Nicolas goy <kuon@goyman.com>
2021-09-15 00:57:01 +0200

    Auth controller tests

--- test/vac_engine_web/controllers/auth_controller_test.exs
+++ test/vac_engine_web/controllers/auth_controller_test.exs
@@ -1,8 +1,44 @@
 defmodule VacEngineWeb.AuthControllerTest do
   use VacEngineWeb.ConnCase
 
+  alias VacEngine.Auth
+  alias VacEngine.Auth.Session
+  alias VacEngine.Repo
+
   test "GET /login", %{conn: conn} do
+    conn = get(conn, "/login")
+    assert html_response(conn, 200) =~ "Login"
+  end
+
+  test "GET /login/:token FAIL", %{conn: conn} do
     conn = get(conn, "/login/12345")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 403) =~ "Access denied"
+  end
+
+  test "GET /login/:token SUCCESS", %{conn: conn} do
+    assert {:ok, %{user: user}} =
+             Auth.create_user(%{
+               email: "test@test.com",
+               name: "Jon Doe",
+               password: "12341234"
+             })
+
+    token =
+      Phoenix.Token.sign(
+        VacEngineWeb.Endpoint,
+        "login_token",
+        {user.id, "/next"}
+      )
+
+    conn = get(conn, "/login/#{token}")
+    assert redirected_to(conn) == "/next"
+
+    assert [session] = Repo.all(Session)
+    assert false == Session.expired?(session)
+
+    conn = get(conn, "/logout")
+    assert html_response(conn, 200) =~ "Logout"
+    assert [session] = Repo.all(Session)
+    assert true == Session.expired?(session)
   end
 end
