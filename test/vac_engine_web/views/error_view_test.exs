5d49ddc0c4cbc46e304cabd107359c68e18fa150
Nicolas goy <kuon@goyman.com>
2021-09-15 00:56:54 +0200

    Error view tests

--- test/vac_engine_web/views/error_view_test.exs
+++ test/vac_engine_web/views/error_view_test.exs
@@ -1,14 +1,13 @@
 defmodule VacEngineWeb.ErrorViewTest do
   use VacEngineWeb.ConnCase, async: true
 
-  # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
-  test "renders 404.html" do
-    assert render_to_string(VacEngineWeb.ErrorView, "404.html", []) == "Not Found"
-  end
-
-  test "renders 500.html" do
-    assert render_to_string(VacEngineWeb.ErrorView, "500.html", []) == "Internal Server Error"
+  # check that error pages are static html files
+  test "renders XXX.html" do
+    for err <- [401, 403, 404, 500] do
+      assert render_to_string(VacEngineWeb.ErrorView, "#{err}.html", []) ==
+               File.read!("lib/vac_engine_web/templates/error/#{err}.html.eex")
+    end
   end
 end
