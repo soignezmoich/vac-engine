f35be1ea80b1b6186690bd78303286bab49c12e3
Nicolas goy <kuon@goyman.com>
2021-09-17 03:52:44 +0200

    Use live flash instead of regular flash

--- lib/vac_engine_web/router.ex
+++ lib/vac_engine_web/router.ex
@@ -5,7 +5,7 @@ defmodule VacEngineWeb.Router do
     plug(RemoteIp)
     plug(:accepts, ["html"])
     plug(:fetch_session)
-    plug(:fetch_flash)
+    plug(:fetch_live_flash)
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
     plug(:fetch_role_session)
