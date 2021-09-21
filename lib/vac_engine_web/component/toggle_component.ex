18b970f3f424f5cccbf95d215fb650f171bf0e46
Nicolas goy <kuon@goyman.com>
2021-09-21 17:51:05 +0200

    A simple boolean toggle component

---
+++ lib/vac_engine_web/component/toggle_component.ex
@@ -0,0 +1,31 @@
+defmodule ToggleComponent do
+  use Phoenix.Component
+
+  import VacEngineWeb.FormatHelpers
+
+  def toggle(assigns) do
+    ~H"""
+    <div class="flex border-cream-200 border cursor-pointer relative select-none"
+         phx-click={@click}"
+         phx-value-key={@key}>
+      <%= if @value do %>
+        <div class="w-10 bg-blue-100"></div>
+        <div class="w-10 bg-blue-500 text-center font-bold text-gray-50 py-1">
+          <%= format_bool(@value) %>
+        </div>
+      <% else %>
+        <div class="w-10 bg-cream-500 text-center font-bold text-gray-100 py-1">
+          <%= format_bool(@value) %>
+        </div>
+        <div class="w-10 bg-cream-100"></div>
+      <% end %>
+      <%= if @key == @tooltip_key do %>
+        <div class="absolute bg-cream-200 text-xs p-1 whitespace-nowrap
+                    -bottom-full z-50 border border-cream-600">
+          click again to toggle
+        </div>
+      <% end %>
+    </div>
+    """
+  end
+end
