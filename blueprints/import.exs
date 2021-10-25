288e3403ac07655b21e0953cd75f03c105c5e3cb
Nicolas goy <kuon@goyman.com>
2021-10-25 14:06:06 +0200

    Remove comment

--- blueprints/import.exs
+++ blueprints/import.exs
@@ -1,39 +1,3 @@
-# alias VacEngine.Processor.Blueprint
-# alias VacEngine.Processor
-# alias VacEngine.Repo
-# alias Fixtures.Blueprints
-#
-# Logger.configure(level: :error)
-#
-# Blueprints.blueprints()
-# |> Enum.filter(fn
-#   {:ruleset0, _} -> true
-#   _ -> false
-# end)
-# |> Enum.each(fn {name, br_def} ->
-#   name = to_string(name)
-#
-#   from(b in Blueprint, where: b.name == ^name)
-#   |> Repo.all()
-#   |> Enum.map(fn br ->
-#     Processor.get_blueprint!(br.id)
-#   end)
-#   |> Enum.each(fn br ->
-#     Processor.update_blueprint(br, br_def)
-#     |> case do
-#       {:ok, _} ->
-#         IO.puts("Updated blueprint #{name}")
-#
-#       {:error, changeset} ->
-#         IO.puts("Error in blueprint #{name}")
-#
-#         VacEngineWeb.ErrorHelpers.inspect_changeset(changeset)
-#         |> IO.puts()
-#     end
-#   end)
-# end)
-#
-
 import Ecto.Query
 alias VacEngine.Account
 alias VacEngine.Processor.Blueprint
