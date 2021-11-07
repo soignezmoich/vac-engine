30358a51f103cb2c4f4cea3e1572f8771ac1da71
antoine <ant.morand@gmail.com>
2021-11-07 21:38:32 +0100

    Delete renderable floder

--- lib/vac_engine_web/live/workspace/blueprint_live/renderables/deductions/deduction_list_renderable.ex
+++
@@ -1,9 +0,0 @@
-defmodule VacEngineWeb.Editor.DeductionListRenderable do
-  def build(deductions) do
-    deductions
-    |> Enum.with_index()
-    |> Enum.map(fn {deduction, index} ->
-      {[index, "deductions"], deduction}
-    end)
-  end
-end
