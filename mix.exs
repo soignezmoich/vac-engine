e7059f2c70ca1e1b15661a38dec05de91621727b
Nicolas Goy <kuon@goyman.com>
2021-12-19 16:20:48 +0100

    Update minimum required elixir version

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule VacEngine.MixProject do
     [
       app: :vac_engine,
       version: @version,
-      elixir: "~> 1.7",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
