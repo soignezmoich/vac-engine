b88f6cb16af7b3470dc9c46b83f01f4bfb93b5bc
Nicolas Goy <kuon@goyman.com>
2021-12-23 19:36:44 +0100

    Add error for JSON format

--- lib/vac_engine_web/views/error_view.ex
+++ lib/vac_engine_web/views/error_view.ex
@@ -1,16 +1,27 @@
 defmodule VacEngineWeb.ErrorView do
   use VacEngineWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.html", _assigns) do
-  #   "Internal Server Error"
-  # end
-
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.html" becomes
-  # "Not Found".
   def template_not_found(_template, assigns) do
     render("500.html", assigns)
+  end
+
+  def render("400.json", _assigns) do
+    %{error: "Bad request"}
+  end
+
+  def render("401.json", _assigns) do
+    %{error: "Unauthorized"}
+  end
+
+  def render("403.json", _assigns) do
+    %{error: "Access denied"}
+  end
+
+  def render("404.json", _assigns) do
+    %{error: "Not found"}
+  end
+
+  def render("500.json", _assigns) do
+    %{error: "Server error"}
   end
 end
