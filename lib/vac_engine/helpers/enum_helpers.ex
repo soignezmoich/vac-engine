dfb8093a83e278b672bc93cc67226c0129ced3a1
Nicolas Goy <kuon@goyman.com>
2022-01-25 20:33:31 +0100

    Add flatten_map/unflatten_map and simple deep merge

--- lib/vac_engine/helpers/enum_helpers.ex
+++ lib/vac_engine/helpers/enum_helpers.ex
@@ -3,6 +3,8 @@ defmodule VacEngine.EnumHelpers do
   Set of Enum helpers
   """
 
+  import VacEngine.PipeHelpers, only: [rpair: 2]
+
   @doc """
   Nil safe get, return nil if map is nil
   """
@@ -52,5 +54,57 @@ defmodule VacEngine.EnumHelpers do
     Enum.find(list, fn el ->
       Map.get(el, key) == value
     end)
+  end
+
+  @doc """
+  Convert a nested map to an array of tuple with dotted path
+  """
+  def flatten_map(m, stack \\ [])
+
+  def flatten_map(m, stack) when is_map(m) do
+    Enum.reduce(m, [], fn {k, v}, acc ->
+      flatten_map(v, stack ++ [k]) ++ acc
+    end)
+  end
+
+  def flatten_map(val, stack) do
+    [{stack, val}]
+  end
+
+  @doc """
+  Convert a flat array to a nested map
+  """
+  def unflatten_map(a) when is_list(a) do
+    Enum.reduce(a, %{}, fn {k, v}, acc ->
+      path = k
+
+      path
+      |> Enum.drop(-1)
+      |> Enum.reduce({acc, []}, fn el, {acc, path} ->
+        path = path ++ [el]
+
+        update_in(acc, path, fn current ->
+          current || %{}
+        end)
+        |> rpair(path)
+      end)
+      |> elem(0)
+      |> put_in(path, v)
+    end)
+  end
+
+  @doc """
+  Simple deep merge of map of map
+  """
+  def sdmerge(a, b) do
+    Map.merge(a, b, &sdresolve/3)
+  end
+
+  defp sdresolve(_key, a, b) when is_map(a) and is_map(b) do
+    sdmerge(a, b)
+  end
+
+  defp sdresolve(_key, _a, b) do
+    b
   end
 end
