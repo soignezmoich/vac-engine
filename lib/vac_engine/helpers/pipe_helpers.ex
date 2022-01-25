58109cab31a4ef346c509cdc3b0d67aecb69b34e
Nicolas Goy <kuon@goyman.com>
2022-01-25 20:32:38 +0100

    Add rpair

--- lib/vac_engine/helpers/pipe_helpers.ex
+++ lib/vac_engine/helpers/pipe_helpers.ex
@@ -18,6 +18,13 @@ defmodule VacEngine.PipeHelpers do
   end
 
   @doc """
+  Wrap into tuple rpair
+  """
+  def rpair(val, res) do
+    {val, res}
+  end
+
+  @doc """
   Tap only if ok tuple
   """
   def tap_ok({:ok, ok_val} = result, fun) do
