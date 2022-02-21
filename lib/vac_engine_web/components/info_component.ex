64d20de66b70afa78b3e5fec548f51e22b350f25
antoine <ant.morand@gmail.com>
2022-02-21 18:27:01 +0100

    remove bottom margin of info_component

--- lib/vac_engine_web/components/info_component.ex
+++ lib/vac_engine_web/components/info_component.ex
@@ -5,7 +5,7 @@ defmodule VacEngineWeb.InfoComponent do
 
   def info_component(assigns) do
     ~H"""
-    <div class="text-xs pr-4 pl-2 py-2 border mb-4 max-w-4xl flex flex-row bg-teal-50">
+    <div class="text-xs pr-4 pl-2 py-2 border max-w-4xl flex flex-row bg-teal-50">
       <div class="pr-1 pt-1.5 text-teal-500">
         <.icon name="hero/information-circle" width="1.2rem" />
       </div>
